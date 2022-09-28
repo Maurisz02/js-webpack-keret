f9f4617af4667e58068b238ee0924d42226cf7fa
Maurisz <p2002adam@gmail.com>
2022-09-28 12:28:08 +0200

    url working

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 
 
+function urlChange(){
+    let url = document.getElementById('inputUrl').value;
+    document.getElementById('imgPlace').src = url;
+}
+
 function init(){
-    
+    document.getElementById('inputUrl').addEventListener('input',urlChange);
 }
 
 document.addEventListener('DOMContentLoaded',init);
